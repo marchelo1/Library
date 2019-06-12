3cd538e59b1da6ba1cb154ddb59739a8fb83da54
marchelo1 <marchelo1_@msn.com>
2019-06-12 12:24:15 +0200

    editing index.ejs

--- server.js
+++ server.js
@@ -1,46 +1,48 @@
 //checking if we are producing in our local environment
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-};
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
 //express server
-const express = require('express');
+const express = require("express");
 const app = express();
-const expressLayouts = require('express-ejs-layouts');
-const bodyParser = require('body-parser')
+const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
 
 // export our Router from /routes/index.js
-const indexRouter = require('./routes/index');
+const indexRouter = require("./routes/index");
 // export our Router from /routes/authors
-const authorRouter = require('./routes/authors');
+const authorRouter = require("./routes/authors");
 // export our Router from /routes/books
-const bookRouter = require('./routes/books');
+const bookRouter = require("./routes/books");
 
-app.set('view engine', 'ejs'); // setting view engine to generate HTML with plain JavaScript, ejs 
-app.set('views', __dirname + '/views'); // from directory views
-app.set('layout', 'layouts/layout'); // putting all our html file, as header and footer in one file
+app.set("view engine", "ejs"); // setting view engine to generate HTML with plain JavaScript, ejs
+app.set("views", __dirname + "/views"); // from directory views
+app.set("layout", "layouts/layout"); // putting all our html file, as header and footer in one file
 app.use(expressLayouts); // use layouts
-app.use(express.static('public')); // in public folder we put css, js, img etc
-app.use(bodyParser.urlencoded({
-    limit: '10mb',
+app.use(express.static("public")); // in public folder we put css, js, img etc
+app.use(
+  bodyParser.urlencoded({
+    limit: "10mb",
     extended: false
-})); // easy to access to the different input element from server
+  })
+); // easy to access to the different input element from server
 
 //conecting our application with mongodb database
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true
+  useNewUrlParser: true
 });
 //checking if we are connecting or not to a database
 const db = mongoose.connection;
-db.on('error', error => console.error(error));
-db.once('open', () => console.log('Connected to Mongoose'));
+db.on("error", error => console.error(error));
+db.once("open", () => console.log("Connected to Mongoose"));
 
 // telling the root path to our application, telling what route to handle
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 // using authorRouter from authors folder
-app.use('/authors', authorRouter);
+app.use("/authors", authorRouter);
 // using authorRouter from books folder
-app.use('/books', bookRouter);
+app.use("/books", bookRouter);
 
 app.listen(process.env.PORT || 3000); // server will listen port 3000
